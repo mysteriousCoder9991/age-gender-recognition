28cd8a18f1418b91f81b7bbb854ca5b9efdf3c49
Rahul Dhar <rdhar9991@gmail.com>
2021-05-09 18:51:38 +0530

    Updated display

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import os
 import sys
 
 from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 
 
 class App(QWidget):
 
     def __init__(self):
         super().__init__()
-        self.title = 'Age-Gender-Detection '
+        self.title = 'Age - Gender Detector 2.0'
         self.left = 500
         self.top = 250
         self.width = 420
         self.height = 150
         self.setStyleSheet("background-color: cyan;")
+        self.setMaximumSize(420, 150)
 
         self.initUI()
 
@@ -22,17 +23,20 @@ class App(QWidget):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
-        startButton = QPushButton('Start the Application', self)
-        exitButton = QPushButton('Exit the Application', self)
+        startButton = QPushButton('Start', self)
+        exitButton = QPushButton('Exit', self)
+        footerLabel = QLabel(" © Rahul Dhar , 2021 ", self)
 
         startButton.setStyleSheet("background-color: white;")
         exitButton.setStyleSheet("background-color: white;")
+        footerLabel.setStyleSheet("background-color:#808080;")
 
-        startButton.setToolTip('This is start button')
-        exitButton.setToolTip('This is the exit button')
+        startButton.setToolTip('To start the application please click this')
+        exitButton.setToolTip('To exit from the application please click this')
 
-        startButton.move(150, 50)
-        exitButton.move(150, 80)
+        startButton.move(175, 50)
+        exitButton.move(175, 80)
+        footerLabel.move(5, 135)
 
         startButton.clicked.connect(self.on_click)
         exitButton.clicked.connect(self.on_click1)
